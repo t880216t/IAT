2e5f8eb831908d89737b633f3317959a7234fc00
chenjian0112 <chenjian0112@made-in-china.com>
2019-08-02 17:07:18 +0800

    1.项目切换后页面清空

--- src/pages/Case/index.js
+++ src/pages/Case/index.js
@@ -359,6 +359,7 @@ export default class CasePage extends PureComponent {
     this.setState({ project },
       () => {
         this.queryTreeList(project);
+        this.clearSelect();
       },
     );
   }
