541fdf0f8221c42134cfd3305f94a1fcdea91be0
chenjian0112 <chenjian0112@made-in-china.com>
2022-05-13 21:42:37 +0800

    调整工程

--- Server/run.py
+++
@@ -1,9 +0,0 @@
-from app import app,socketio
-import subprocess
-
-def runTimingCheck():
-    subprocess.Popen('python runTimingCheck.py main',shell=True)
-
-if __name__ == '__main__':
-  runTimingCheck()
-  socketio.run(app,host='0.0.0.0', port=5000, debug=True, use_reloader=False)
--- UI/src/layouts/BlankLayout.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Layout = ({ children }) => <div>{children}</div>;
-
-export default Layout;
